d37d9e668be3ce8ddcb0db44189633b87e71a614
Ryan Soderberg <ryanjsoderberg@gmail.com>
2025-01-25 20:06:07 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { generateSitemapPlugin as generateSitemap } from "./generate";
+import { Sitemap, CommonSitemapFields } from "./sitemap-types";
+
+export { generateSitemap, Sitemap, CommonSitemapFields };
